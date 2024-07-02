ee842947ae5d37d1339fbf9835b037abf09c0e91
JL-20191210 <534457091@qq.com>
2024-07-02 17:36:10 +0800

    封装表单对话框

---
+++ weblog-vue3/src/api/admin/category.js
@@ -0,0 +1,17 @@
+import axios from "@/axios"
+
+// 获取分类分页参数
+export function getCategoryPageList(data){
+    return axios.post("/admin/category/list",data)
+}
+
+// 添加分类
+export function addCategory(data){
+    return axios.post("/admin/category/add",data)
+}
+
+// 删除分类
+export function deleteCategory(id){
+    // 包装成了一个对象的形式：{id : id} ， {id} 是其简写模式
+    return axios.post("/admin/category/delete",{id})
+}
---
+++ weblog-vue3/src/components/FormDialog.vue
@@ -0,0 +1,54 @@
+<template>
+    <el-dialog v-model="dialogVisible" :title="title" :width="width" :destroy-on-close="destroyOnClose"  :draggable="true" :close-on-click-modal="false"
+        :close-on-press-escape="false">
+        <!-- 插槽 -->
+        <slot></slot>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="dialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="submit">
+                    {{confirmText}}
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+const dialogVisible = ref(false)
+
+// 打开
+const open = () => dialogVisible.value = true
+// 关闭
+const close = () => dialogVisible.value = false
+
+// 暴露给父组件
+defineExpose({
+    open,
+    close
+})
+
+// 对外暴露属性
+const props = defineProps({
+    title: String,
+    width:{
+        type:String,
+        default:'40%'
+    },
+    destroyOnClose:{
+        type:Boolean,
+        default:false
+    },
+    confirmText:{
+        type:String,
+        default:'提交'
+    }
+})
+
+// 对外暴露一个submit方法
+const emit = defineEmits(['submit'])
+//'submit'是子组件的事件，submit是暴露出去让父组件进行监听，使用emit函数声明子组件发出的事件。简言之，使用emit可以让父组件监听到子组件中的提交按钮被点击了
+const submit = ()=>emit('submit')
+</script>
