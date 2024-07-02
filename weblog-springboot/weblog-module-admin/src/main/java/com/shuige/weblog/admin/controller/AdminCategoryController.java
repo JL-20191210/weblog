9947ecd26bb556ed0b8f31ddcfdac2d9293108a0
JL-20191210 <534457091@qq.com>
2024-07-02 09:50:37 +0800

    覆盖恢复

--- weblog-springboot/weblog-module-admin/src/main/java/com/shuige/weblog/admin/controller/AdminCategoryController.java
+++ weblog-springboot/weblog-module-admin/src/main/java/com/shuige/weblog/admin/controller/AdminCategoryController.java
@@ -1,8 +1,11 @@
 package com.shuige.weblog.admin.controller;
 
 import com.shuige.weblog.admin.model.vo.category.AddCategoryReqVO;
+import com.shuige.weblog.admin.model.vo.category.DeleteCategoryReqVO;
+import com.shuige.weblog.admin.model.vo.category.FindCategoryPageListReqVO;
 import com.shuige.weblog.admin.service.AdminCategoryService;
 import com.shuige.weblog.common.aspect.ApiOperationLog;
+import com.shuige.weblog.common.utils.PageResponse;
 import com.shuige.weblog.common.utils.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -13,23 +16,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author felix
- * @date 2024/7/1 14:34
- */
 @RestController
 @RequestMapping("/admin")
-@Api(tags = "Admin分类模块")
-@SuppressWarnings("all")
+@Api(tags = "Admin 分类模块")
 public class AdminCategoryController {
 
     @Autowired
-    private AdminCategoryService adminCategoryService;
+    private AdminCategoryService categoryService;
 
     @PostMapping("/category/add")
     @ApiOperation(value = "添加分类")
     @ApiOperationLog(description = "添加分类")
-    public Response addCategory(@RequestBody @Validated AddCategoryReqVO addCategoryReqVo){
-        return adminCategoryService.addCategory(addCategoryReqVo);
+    public Response addCategory(@RequestBody @Validated AddCategoryReqVO addCategoryReqVO) {
+        return categoryService.addCategory(addCategoryReqVO);
+    }
+
+
+    @PostMapping("/category/list")
+    @ApiOperation(value = "查询分类列表分页数据")
+    @ApiOperationLog(description = "分类分页数据获取")
+    public PageResponse findCategoryPageList(@RequestBody @Validated FindCategoryPageListReqVO findCategoryPageListReqVO) {
+        return categoryService.findCategoryPageList(findCategoryPageListReqVO);
+    }
+
+    @PostMapping("/category/delete")
+    @ApiOperation(value = "删除分类")
+    @ApiOperationLog(description = "删除分类")
+    public Response deleteCategory(@RequestBody @Validated DeleteCategoryReqVO deleteCategoryReqVO) {
+        return categoryService.deleteCategory(deleteCategoryReqVO);
+    }
+
+    @PostMapping("/category/select/list")
+    @ApiOperation(value = "分类 Select 下拉列表数据获取")
+    @ApiOperationLog(description = "分类 Select 下拉列表数据获取")
+    public Response findCategorySelectList() {
+        return categoryService.findCategorySelectList();
     }
 }
